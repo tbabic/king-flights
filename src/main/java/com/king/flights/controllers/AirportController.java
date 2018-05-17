d85b87bde195cd77841746313470935ef4b4d282
Tomislav Babic <tomislav.babic@fleksbit.hr>
2018-05-17 07:18:47 +0200

    cleaning up

--- src/main/java/com/king/flights/controllers/AirportController.java
+++ src/main/java/com/king/flights/controllers/AirportController.java
@@ -3,7 +3,6 @@ package com.king.flights.controllers;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
--- src/main/java/com/king/flights/repositories/AirportRepository.java
+++ src/main/java/com/king/flights/repositories/AirportRepository.java
@@ -1,6 +1,5 @@
 package com.king.flights.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.king.flights.models.Airport;
